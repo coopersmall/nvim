205f6ed4f7c333a0c9f396f2a88ed94fa8d37f04
Cooper-Small <cooper.small@gladly.com>
2023-04-25 18:08:38 -0500

    added some stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,49 +1,61 @@
 local lsp = require('lsp-zero')
 
-lsp.preset("lsp-compe")
+lsp.preset({})
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'sumneko_lua',
-  'rust_analyzer',
-  'gopls',
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({select = true}),
-  ['<C-Space>'] = cmp.mapping.complete(),
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'rust_analyzer',
+    'gopls',
 })
 
 lsp.set_preferences({
-  sign_icons = { }
+    sign_icons = {}
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set('n', 'gb', function()vim.lsp.buf.definition() end, opts)
-  vim.keymap.set('n', 'gk', function()vim.lsp.buf.hover() end, opts)
-  vim.keymap.set('n', '<leader>vws', function()vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set('n', '<leader>vd', function()vim.diagnostic.open_float() end, opts)
-  vim.keymap.set('n', '[d', function()vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set('n', ']d', function()vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set('n', '<leader>vca', function()vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set('n', '<leader>vrr', function()vim.lsp.buf.references() end, opts)
-  vim.keymap.set('n', '<leader>vrn', function()vim.lsp.buf.rename() end, opts)
-  vim.keymap.set('n', '<C-h>', function()vim.lsp.buf.signature_help() end, opts)
-
+    vim.keymap.set('n', 'gb', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gk', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.format_on_save({
     servers = {
-        ['lua-ls'] = {'lua'},
-        ['rust_analyzer'] = {'rust'}
+        ['lua_ls'] = { 'lua' },
+        ['gopls'] = { 'go' },
+        ['rust_analyzer'] = { 'rust' }
     }
 })
 
 lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    mapping = {
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<S-l>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<S-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    },
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'buffer',  keyword_length = 3 },
+        { name = 'luasnip', keyword_length = 2 },
+    }
+})
--- lua/coop/plugins.lua
+++ lua/coop/plugins.lua
@@ -10,7 +10,11 @@ return require('packer').startup(function(use)
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { {'nvim-lua/plenary.nvim'}, {'nvim-telescope/telescope-live-grep-args.nvim' },
+      },
+      config = function()
+          require("telescope").load_extension("live_grep_args")
+      end
   }
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
@@ -27,6 +31,7 @@ return require('packer').startup(function(use)
   use('mbbill/undotree')
 
   use('tpope/vim-fugitive')
+
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v2.x',
@@ -45,6 +50,9 @@ return require('packer').startup(function(use)
 	  {'hrsh7th/nvim-cmp'},     -- Required
 	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
 	  {'L3MON4D3/LuaSnip'},     -- Required   
-  }
+  },
 }
+
+use ('fatih/vim-go')
+
 end)
