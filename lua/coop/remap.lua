3fb8a161b09b6e4f042e00b13b7dce158ae4c80d
Cooper-Small <cooper.small@gladly.com>
2023-06-09 13:32:39 -0600

    add some remaps

--- lua/coop/remap.lua
+++ lua/coop/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<C-o>", vim.cmd.Ex)
-vim.keymap.set("n", "<C-p>", "<cmd>visual<CR>")
+vim.keymap.set("n", "<C-p>", "<CMD>visual<CR>")
 
 vim.keymap.set("n", '<leader>pv', ':Ex<CR>')
 vim.keymap.set("n", '<leader>u', ':UndotreeShow<CR>')
@@ -32,34 +32,47 @@ vim.keymap.set('n', '<leader>Y', "\"+Y")
 vim.keymap.set('n', '<leader>d', "\"_d")
 vim.keymap.set('v', '<leader>d', "\"_d")
 
-vim.keymap.set('n', '<C-k>', "<cmd>cnext<CR>zz")
-vim.keymap.set('n', '<C-j>', "<cmd>cprev<CR>zz")
-vim.keymap.set('n', '<leader>k', "<cmd>lnext<CR>zz")
-vim.keymap.set('n', '<leader>j', "<cmd>lprev<CR>zz")
+vim.keymap.set('n', '<C-k>', "<CMD>cnext<CR>zz")
+vim.keymap.set('n', '<C-j>', "<CMD>cprev<CR>zz")
+vim.keymap.set('n', '<leader>k', "<CMD>lnext<CR>zz")
+vim.keymap.set('n', '<leader>j', "<CMD>lprev<CR>zz")
 
 
 vim.keymap.set('n', '<leader>s', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set('n', '<leader>x', "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set('n', '<leader>x', "<CMD>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set('n', '<leader>cd', ":cd %:p:h<CR>:pwd<CR>")
 
 vim.keymap.set('v', '<C-i>', "<Esc>")
 vim.keymap.set('i', '<C-i>', "<Esc>")
 
-vim.keymap.set('n', '<C-q>', "<cmd>q!<CR>")
-vim.keymap.set('n', '<C-s>', "<cmd>w<CR>")
+vim.keymap.set('n', '<C-q>', "<CMD>q!<CR>")
+vim.keymap.set('n', '<C-s>', "<CMD>w<CR>")
 
-vim.keymap.set('n', '<leader>cr', "<cmd>lua Colors()<CR>")
+vim.keymap.set('n', '<leader>cr', "<CMD>lua Colors()<CR>")
 
-vim.keymap.set('n', '<C-c>', "<cmd>bdelete<CR>")
-vim.keymap.set('n', '<C-n>', "<cmd>enew<CR>")
+vim.keymap.set('n', '<C-c>', "<CMD>bdelete<CR>")
+vim.keymap.set('n', '<C-n>', "<CMD>enew<CR>")
 
-vim.keymap.set('n', '<S-h>', "<cmd>bprev<CR>")
-vim.keymap.set('n', "<S-l>", "<cmd>bnext<CR>")
+vim.keymap.set('n', '<S-h>', "<CMD>bprev<CR>")
+vim.keymap.set('n', "<S-l>", "<CMD>bnext<CR>")
 
-vim.keymap.set('n', '<leader>cr', "<cmd>lua Colors()<CR>")
+vim.keymap.set('n', '<leader>cr', "<CMD>lua Colors()<CR>")
 
 vim.keymap.set('n', '<leader>o', "<C-o>")
 vim.keymap.set('n', '<leader>i', "<C-i>")
 
-vim.keymap.set('n', '<C-q>', "<cmd>qa<CR>")
+vim.keymap.set('n', '<C-q>', "<CMD>qa<CR>")
+
+vim.keymap.set('n', '<leader>yy', '"*yy')
+vim.keymap.set('n', '<leader>yiw', '"*yiw')
+vim.keymap.set('n', '<leader>yi"', '"*yi"')
+vim.keymap.set('n', '<leader>yi(', '"*yi(')
+vim.keymap.set('n', '<leader>yi{', '"*yi{')
+
+vim.keymap.set('n', '<C-o>', 'o<Esc>')
+vim.keymap.set('n', '<C-O>', 'O<Esc>')
+
+vim.keymap.set('n', '<leader>h', '<CMD>noh<CR>')
+
+vim.keymap.set('n', '<leader>lsp', '<CMD>LspStop<CR><CMD>LspStart<CR>')
